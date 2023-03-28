d119e39119e524fade0a7595fba04031a2040f1e
Thomas-DaPrato <daprato.thomas05@gmail.com>
2023-03-28 08:59:52 +0200

    fin camera manager

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public Transform player1;
+    public Transform player2;
+
+
+    void Update() {
+        gameObject.transform.position = CameraMovement();
+    }
+
+    public Vector3 CameraMovement() {
+        double distanceBetweenPlayer = Math.Sqrt(Math.Pow(player2.position.x - player1.position.x, 2) + Math.Pow(player2.position.y - player1.position.y, 2));
+        double cameraZ;
+        if (distanceBetweenPlayer <= 8)
+            cameraZ = -5;
+        else
+            cameraZ = -1 - distanceBetweenPlayer / 2;
+        return new Vector3((player2.position.x + player1.position.x)/2, (player2.position.y + player1.position.y)/2, (float) cameraZ);
+    }
+}
