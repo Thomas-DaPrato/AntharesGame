cce68e64f628af0be1fde466118a700579978ac8
Thomas-DaPrato <daprato.thomas05@gmail.com>
2023-10-03 08:42:58 +0200

    first version of player movement : add velocity

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
     private CharacterController controller;
     private Vector3 playerVelocity;
+    private Vector3 playerControllerVelocity;
     private bool groundedPlayer;
+
+    private float lastDirection; 
 
     public bool isAttacking = false;
     public bool isParrying = false;
@@ -20,7 +24,9 @@ public class PlayerController : MonoBehaviour
     public FighterData GetFighterData() { return fighterData; }
 
     [SerializeField]
-    private float playerSpeed = 2.0f;
+    private float playerSpeed = 6.0f;
+    [SerializeField]
+    private float playerMaxSpeed = 7.0f;
     [SerializeField]
     private float jumpHeight = 1.0f;
     [SerializeField]
@@ -35,54 +41,11 @@ public class PlayerController : MonoBehaviour
 
     private int hp = 10;
 
-    private void Start() {
+    private void Awake() {
         controller = gameObject.GetComponent<CharacterController>();
-        
-    }
-
-    public void OnMove(InputAction.CallbackContext context) {
-        if(!isStunt)
-            direction = context.ReadValue<float>();
-    }
-
-    public void OnHeavyAttack(InputAction.CallbackContext context) {
-        if (context.performed && !isAttacking && !isStunt) {
-            Debug.Log(gameObject.name + " Heavy");
-            isAttacking = true;
-            animator.SetTrigger("HeavyAttack");
-        }
     }
    
-    public void OnMiddleAttack(InputAction.CallbackContext context) {
-        if (context.performed && !isAttacking && !isStunt){
-            Debug.Log(gameObject.name + " Middle");
-            isAttacking = true;
-            animator.SetTrigger("MiddleAttack");
-        }
-    }
-    public void OnLightAttack(InputAction.CallbackContext context) {
-        if (context.performed && !isAttacking && !isStunt){
-            Debug.Log(gameObject.name + " Light");
-            isAttacking = true;
-            animator.SetTrigger("LightAttack");
-        }
-        
-    }
-    public void OnParry(InputAction.CallbackContext context) {
-        if (context.performed && !isAttacking && !isStunt){
-            Debug.Log(gameObject.name + " Parry");
-            isParrying = true;
-            animator.SetTrigger("Parry");
-        }
-        
-    }
-
-    public void OnJump(InputAction.CallbackContext context) {
-        if(!isStunt)
-            jump = context.action.triggered;
-    }
-
-    void Update() {
+    void FixedUpdate() {
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0) {
             playerVelocity.y = 0f;
@@ -96,14 +59,79 @@ public class PlayerController : MonoBehaviour
         controller.Move(move * Time.deltaTime * playerSpeed);
 
 
-
         // Changes the height position of the player..
         if (jump && groundedPlayer)
-            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            playerVelocity.y += jumpHeight * 3;
 
         playerVelocity.y += gravityValue * Time.deltaTime;
+
+
+        if (!groundedPlayer)
+            if (lastDirection > 0)
+                if (playerVelocity.x < playerMaxSpeed)
+                    playerVelocity.x += controller.velocity.x * Time.deltaTime * playerSpeed;
+                else
+                    playerVelocity.x = playerMaxSpeed;
+
+            else
+                if (playerVelocity.x > -playerMaxSpeed)
+                    playerVelocity.x += controller.velocity.x * Time.deltaTime * playerSpeed;
+                else
+                    playerVelocity.x = -playerMaxSpeed;
+        else
+            playerVelocity.x = 0;
+
+        
+
         controller.Move(playerVelocity * Time.deltaTime);
 
+    }
+
+
+    public void OnMove(InputAction.CallbackContext context) {
+        if (!isStunt) {
+            direction = context.ReadValue<float>();
+            if (direction != 0)
+                lastDirection = direction;
+        }
+
+    }
+
+    public void OnHeavyAttack(InputAction.CallbackContext context) {
+        if (context.performed && !isAttacking && !isStunt) {
+            Debug.Log(gameObject.name + " Heavy");
+            isAttacking = true;
+            animator.SetTrigger("HeavyAttack");
+        }
+    }
+
+    public void OnMiddleAttack(InputAction.CallbackContext context) {
+        if (context.performed && !isAttacking && !isStunt) {
+            Debug.Log(gameObject.name + " Middle");
+            isAttacking = true;
+            animator.SetTrigger("MiddleAttack");
+        }
+    }
+    public void OnLightAttack(InputAction.CallbackContext context) {
+        if (context.performed && !isAttacking && !isStunt) {
+            Debug.Log(gameObject.name + " Light");
+            isAttacking = true;
+            animator.SetTrigger("LightAttack");
+        }
+
+    }
+    public void OnParry(InputAction.CallbackContext context) {
+        if (context.performed && !isAttacking && !isStunt) {
+            Debug.Log(gameObject.name + " Parry");
+            isParrying = true;
+            animator.SetTrigger("Parry");
+        }
+
+    }
+
+    public void OnJump(InputAction.CallbackContext context) {
+        if (!isStunt)
+            jump = context.action.triggered;
     }
 
     public void TakeDamage(int damage) {
