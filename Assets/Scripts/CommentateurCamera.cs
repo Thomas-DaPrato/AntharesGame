cc37c727c3248d57e635223a30f90c62708ea118
RAHAL Rayane <91078977+ReyDioGames@users.noreply.github.com>
2023-12-06 15:15:44 +0100

    liste commentaire début du match
    j'ai remplacé l'audio du start du match par une liste

--- Assets/Scripts/CommentateurCamera.cs
+++ Assets/Scripts/CommentateurCamera.cs
@@ -6,10 +6,25 @@ public class CommentateurCamera : MonoBehaviour
 {
     public List<AudioClip> commentateurAudioCoups;
     public List<AudioClip> commentateurAudioPieges;
+    public List<AudioClip> commentateurStart;
+
     public AudioSource audioSource;
     private bool isCommenting = false;
-    
 
+    private void Start()
+    {
+        
+        if (!isCommenting)
+        {
+            int r = Random.Range(0, commentateurStart.Count);
+            AudioClip clip = commentateurStart[r];
+            audioSource.clip = clip;
+            audioSource.Play();
+            isCommenting = true;
+            //arret
+        }
+
+    }
 
     public void CommentateurPiege()
     {
