3ad3aa13cbfca3e714f0c9ebbbe3de21f3dc67f4
RAHAL Rayane <91078977+ReyDioGames@users.noreply.github.com>
2024-01-07 15:50:38 +0100

    commentateur en fonction des pieges et presentation des joueur

--- Assets/Scripts/CommentateurCamera.cs
+++ Assets/Scripts/CommentateurCamera.cs
@@ -5,47 +5,103 @@ using UnityEngine;
 public class CommentateurCamera : MonoBehaviour
 {
     public List<AudioClip> commentateurAudioCoups;
-    public List<AudioClip> commentateurAudioPieges;
+    public List<AudioClip> commentateurAudioPiegesFlammes;
+    public List<AudioClip> commentateurAudioPiegesGeyser;
+    public List<AudioClip> commentateurAudioPiegesScie;
+    public List<AudioClip> commentateurAudioPiegesLaser;
+    public List<AudioClip> commentateurMirrorMatch;
+    public List<AudioClip> commentateurPresentationDiane;
+    public List<AudioClip> commentateurPresentationCesar;
     public List<AudioClip> commentateurStart;
+    public List<AudioClip> commentateurBienvenu;
 
     public AudioSource audioSource;
+    [SerializeField] private GameObject player1;
+    [SerializeField] private GameObject player2;
+    [SerializeField] private GameObject flammes;
+    [SerializeField] private GameObject geyser;
+    [SerializeField] private GameObject scie;
+    [SerializeField] private GameObject laserD;
+    [SerializeField] private GameObject laserG;
+
     private bool isCommenting = false;
+    private int r=0;
 
     private float dureeAudio=0;
 
     private void Start()
     {
+
+        r = Random.Range(0, commentateurBienvenu.Count - 1);
+        AudioClip clip = commentateurBienvenu[r];
+        dureeAudio = clip.length;
+        audioSource.clip = clip;
+        audioSource.Play();
+        isCommenting = true;
+        StartCoroutine(PresentationJoueur(dureeAudio));
+
         
-        if (!isCommenting)
-        {
-            int r = Random.Range(0, commentateurStart.Count -1);
-            AudioClip clip = commentateurStart[r];
-            dureeAudio = clip.length;
-            audioSource.clip = clip;
-            audioSource.Play();
-            isCommenting = true;
-            StartCoroutine(CanComment(dureeAudio));
-        }
 
     }
 
-    public void CommentateurPiege()
+
+    //il faut rajouter le piege.name comme parametre au moment de l'appel 
+    public void CommentateurPiege(string nomPiege)
     {
-        if (!isCommenting) { 
-            int r = Random.Range(0, commentateurAudioPieges.Count - 1);
-            AudioClip clip = commentateurAudioPieges[r];
-            dureeAudio = clip.length;
-            audioSource.clip = clip;
-            audioSource.Play();
-            isCommenting = true;
-            StartCoroutine(CanComment(dureeAudio));
+        if (!isCommenting) {
+
+            if(nomPiege==laserD.name|| nomPiege == laserG.name)
+            {
+                r = Random.Range(0, commentateurAudioPiegesLaser.Count - 1);
+                AudioClip clip = commentateurAudioPiegesLaser[r];
+                dureeAudio = clip.length;
+                audioSource.clip = clip;
+                audioSource.Play();
+                isCommenting = true;
+                StartCoroutine(CanComment(dureeAudio));
+            }
+            else if (nomPiege == flammes.name)
+            {
+                r = Random.Range(0, commentateurAudioPiegesFlammes.Count - 1);
+                AudioClip clip = commentateurAudioPiegesFlammes[r];
+                dureeAudio = clip.length;
+                audioSource.clip = clip;
+                audioSource.Play();
+                isCommenting = true;
+                StartCoroutine(CanComment(dureeAudio));
+            }
+            else if (nomPiege == scie.name)
+            {
+                r = Random.Range(0, commentateurAudioPiegesScie.Count - 1);
+                AudioClip clip = commentateurAudioPiegesScie[r];
+                dureeAudio = clip.length;
+                audioSource.clip = clip;
+                audioSource.Play();
+                isCommenting = true;
+                StartCoroutine(CanComment(dureeAudio));
+            }
+            else if (nomPiege == geyser.name)
+            {
+                r = Random.Range(0, commentateurAudioPiegesGeyser.Count - 1);
+                AudioClip clip = commentateurAudioPiegesGeyser[r];
+                dureeAudio = clip.length;
+                audioSource.clip = clip;
+                audioSource.Play();
+                isCommenting = true;
+                StartCoroutine(CanComment(dureeAudio));
+            }
+
+            
+            
         }
     }
+
+
     public void CommentateurCoups()
     {
         if (!isCommenting)
         {
-            int r = Random.Range(0, commentateurAudioCoups.Count - 1);
+            r = Random.Range(0, commentateurAudioCoups.Count - 1);
             AudioClip clip = commentateurAudioCoups[r];
             dureeAudio = clip.length;
             audioSource.clip = clip;
@@ -55,10 +111,115 @@ public class CommentateurCamera : MonoBehaviour
         }
     }
 
+
+
     public IEnumerator CanComment(float durée)
     {
         yield return new WaitForSeconds(durée);
+
+
         isCommenting = false;
+    }
+
+
+    public IEnumerator CanCommentstart(float durée)
+    {
+        
+        yield return new WaitForSeconds(durée);
+
+        //pitch debut de match ready fight etc
+        r = Random.Range(0, commentateurStart.Count - 1);
+        AudioClip clip = commentateurStart[r];
+        dureeAudio = clip.length;
+        audioSource.clip = clip;
+        audioSource.Play();
+        isCommenting = true;
+        StartCoroutine(CanComment(dureeAudio));
+    }
+    public IEnumerator CommentDiane(float durée)
+    {
+
+        yield return new WaitForSeconds(durée);
+
+
+        r = Random.Range(0, commentateurPresentationDiane.Count - 1);
+        AudioClip clip = commentateurPresentationDiane[r];
+        dureeAudio = clip.length;
+        audioSource.clip = clip;
+        audioSource.Play();
+        isCommenting = true;
+        StartCoroutine(CanCommentstart(dureeAudio));
+    }
+    public IEnumerator CommentCesar(float durée)
+    {
+
+        yield return new WaitForSeconds(durée);
+
+
+        r = Random.Range(0, commentateurPresentationCesar.Count - 1);
+        AudioClip clip = commentateurPresentationCesar[r];
+        dureeAudio = clip.length;
+        audioSource.clip = clip;
+        audioSource.Play();
+        isCommenting = true;
+        StartCoroutine(CanCommentstart(dureeAudio));
+    }
+
+
+    public IEnumerator PresentationJoueur(float durée)
+    {
+
+        yield return new WaitForSeconds(durée);
+
+
+        if (player1.name == player2.name)
+        {
+            //mirror match
+
+            r = Random.Range(0, commentateurMirrorMatch.Count - 1);
+            AudioClip clip = commentateurMirrorMatch[r];
+            dureeAudio = clip.length;
+            audioSource.clip = clip;
+            audioSource.Play();
+            isCommenting = true;
+            StartCoroutine(CanCommentstart(dureeAudio));
+        }
+        else if (player1.name == "Diane")
+        {   //joueur1 Dianne et joueur2 Cesar
+
+
+            //presentation du joueur 1
+
+            r = Random.Range(0, commentateurPresentationDiane.Count - 1);
+            AudioClip clip = commentateurPresentationDiane[r];
+            dureeAudio = clip.length;
+            audioSource.clip = clip;
+            audioSource.Play();
+            isCommenting = true;
+
+
+            //presentation du joueur 2
+
+            StartCoroutine(CommentCesar(dureeAudio));
+        }
+        else
+        {   //joueur1 Cesar et joueur2 Dianne 
+
+
+            //presentation du joueur 1
+
+            r = Random.Range(0, commentateurPresentationCesar.Count - 1);
+            AudioClip clip = commentateurPresentationCesar[r];
+            dureeAudio = clip.length;
+            audioSource.clip = clip;
+            audioSource.Play();
+            isCommenting = true;
+
+
+            //presentation du joueur 2
+
+            StartCoroutine(CommentDiane(dureeAudio));
+        }
     }
 
 }
