cad271a0c6a723f235d7bf05c238d7bd08e08731
RAHAL Rayane <91078977+ReyDioGames@users.noreply.github.com>
2024-01-07 16:38:58 +0100

    ajout des son a l'UI bouton

--- Assets/Scripts/UI/UI3D/UI3DManager.cs
+++ Assets/Scripts/UI/UI3D/UI3DManager.cs
@@ -19,6 +19,13 @@ public class UI3DManager : MonoBehaviour
     private GameObject vcBat;
 
 
+    public AudioSource audioSource; 
+    public List<AudioClip> audioButtonSwap;
+    public List<AudioClip> audioButtonClick;
+    int r;
+    private float dureeAudio = 0;
+
+
 
 
     private int currentButtonSelected = 0;
@@ -30,7 +37,22 @@ public class UI3DManager : MonoBehaviour
     public void OnSwapButton(InputAction.CallbackContext context) {
 
         if (context.performed) {
-            
+
+            //son du swap button
+            if (audioButtonSwap.Count == 0)
+            {
+                Debug.Log("pas de son swap");
+            }
+            else
+            {
+                r = Random.Range(0, audioButtonSwap.Count - 1);
+                AudioClip clip = audioButtonSwap[r];
+                dureeAudio = clip.length;
+                audioSource.clip = clip;
+                audioSource.Play();
+            }
+
+
             ChangeMaterial(buttons[currentButtonSelected], notSelectedButton);
             if (context.ReadValue<float>() < 0) {
                 currentButtonSelected += 1;
@@ -48,6 +70,22 @@ public class UI3DManager : MonoBehaviour
 
     public void OnClick(InputAction.CallbackContext context) {
         if (context.performed) {
+
+            //son du click button
+            if (audioButtonClick.Count == 0)
+            {
+                Debug.Log("pas de son click");
+            }
+            else
+            {
+                r = Random.Range(0, audioButtonClick.Count - 1);
+                AudioClip clip = audioButtonClick[r];
+                dureeAudio = clip.length;
+                audioSource.clip = clip;
+                audioSource.Play();
+            }
+            
+
             if (!buttons[currentButtonSelected].GetComponent<Click3DButton>().isQuitButton) {
                 gameObject.SetActive(false);
                 buttons[currentButtonSelected].GetComponent<Click3DButton>().DisplayPanel();
